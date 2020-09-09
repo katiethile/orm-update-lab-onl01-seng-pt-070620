c6ba9a042f9966d936e67eb794ccdfac89fe314e
Katie Le <lethikatie@gmail.com>
2020-09-09 06:43:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,4 +66,9 @@ def self.find_by_name(name)
   result = DB[:conn].execute(sql, name)[0]
   Student.new(result[0], result[1], result[2])
   end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.name)
+  end
 end 
