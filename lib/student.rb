1e0ea7d9d49515f0fc01796de5590241052c2324
Katie Le <lethikatie@gmail.com>
2020-09-09 06:45:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,14 +57,14 @@ def self.new_from_db(row)
   new_student
 end 
 
-def self.find_by_name(name)
-  sql = <<-SQL
-  SELECT * FROM students 
-  WHERE name = ?
-  SQL
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students 
+    WHERE name = ?
+    SQL
   
-  result = DB[:conn].execute(sql, name)[0]
-  Student.new(result[0], result[1], result[2])
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
   end
   
   def update
