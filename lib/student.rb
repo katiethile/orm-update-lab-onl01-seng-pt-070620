d2e16dbf14887b5455ca082fc4352c8249eac4ae
Katie Le <lethikatie@gmail.com>
2020-09-09 06:27:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,4 +38,8 @@ def save
   DB[:conn].execute(sql, self.name, self.grade)
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end 
+
+def self.create(name, grade)
+  
+end 
 end
