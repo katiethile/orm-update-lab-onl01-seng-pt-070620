7fe29f2efa1e7db9fb8fe4cbfd23229126c1d13a
Katie Le <lethikatie@gmail.com>
2020-09-09 06:41:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ def save
     self.update
   else
     sql = <<-SQL
-      INSERT INTO songs (name, grade)
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
