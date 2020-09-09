23494c2a20997f3e0c269ed18bb7952ce44ded04
Katie Le <lethikatie@gmail.com>
2020-09-09 06:50:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,13 +49,13 @@ def self.create(name, grade)
   student
 end 
 
-def self.new_from_db(row)
-  new_student = self.new
-  new_student.id = row[0]
-  new_student.name[1]
-  new_student.grade[2]
-  new_student
-end 
+ def self.new_from_db(row)
+    new_student = self.new 
+    new_student.id = row [0]
+    new_student.name = row [1]
+    new_student.grade = row [2]
+    new_student
+  end 
 
   def self.find_by_name(name)
     sql = <<-SQL
